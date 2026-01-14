82cd265565bcdf005ccf8d1a82e7c95b054da2a2
VU Khac Tri <khactri.vu@sony.com>
2026-01-14 08:28:18 +0100

    test: update tests, demos

---
+++ test_crlf.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MIT
+# Copyright 2026 Sony Group Corporation
+# Author: R&D Center Europe Brussels Laboratory, Sony Group Corporation
+# License: For licensing see the License.txt file
+
+# Copyright 2026 Sony Group Corporation
+# Author: R&D Center Europe Brussels Laboratory, Sony Group Corporation
+# License: For licensing see the License.txt file
+
+
+﻿print('CRLF test')
+print('more code')
