466077f30ef65e6efd45afd44539ad33cfc1979a
VU Khac Tri <khactri.vu@sony.com>
2026-01-15 08:56:19 +0100

    Add test file with old copyright

---
+++ test_per_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# SPDX-License-Identifier: MIT
+# Copyright 2020-2026 Sony Group Corporation
+# Author: R&D Center Europe Brussels Laboratory, Sony Group Corporation
+# License: For licensing see the License.txt file
+
+# Copyright 2020-2023 Sony Group Corporation
+# Author: R&D Center Europe Brussels Laboratory, Sony Group Corporation
+
+def old_copyright_per_file_test():
+    """Testing per-file-years mode."""
+    pass
